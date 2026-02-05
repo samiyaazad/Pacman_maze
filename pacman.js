572da9df41e5910ab73e96e94531f4dbc4ff9fed
samiyaazad <samiya16azad@gmail.com>
2026-02-05 09:19:37 +0600

    Pacman maze
    Just a vibing game

--- pacman.js
+++ pacman.js
@@ -20,17 +20,161 @@ let wallImage;
 
 window.onload = function()
 {
-    board = document.getElementByID("board");
+    board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d");
+
+    loadImages();
+    loadMap();
+    //console.log(walls.size);
+    //console.log(foods.size);
+    //console.log(ghosts.size);
+    update();
 }
+
+//X = wall, O = skip, P = pac man, ' ' = food
+//Ghosts: b = blue, o = orange, p = pink, r = red
+const tileMap = [
+    "XXXXXXXXXXXXXXXXXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X                 X",
+    "X XX X XXXXX X XX X",
+    "X    X       X    X",
+    "XXXX XXXX XXXX XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXrXX X XXXX",
+    "O       bpo       O",
+    "XXXX X XXXXX X XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXXXX X XXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X  X     P     X  X",
+    "XX X X XXXXX X X XX",
+    "X    X   X   X    X",
+    "X XXXXXX X XXXXXX X",
+    "X                 X",
+    "XXXXXXXXXXXXXXXXXXX" 
+];
+
+const walls = new Set();
+const foods = new Set();
+const ghosts = new Set();
+let pacman;
+
 
 function loadImages()
 {
     wallImage = new Image ();
     wallImage.src = "./wall.png";
 
-    
+    blueGhostImage = new Image();
+    blueGhostImage.src = "./blueGhost.png"
 
+    redGhostImage = new Image ();
+    redGhostImage.src = "./redGhost.png"
+
+    pinkGhostImage = new Image ();
+    pinkGhostImage.src = "./pinkGhost.png"
+
+    orangeGhostImage = new Image ();
+    orangeGhostImage.src = "./orangeGhost.png"
+
+    pacmanUpImage = new Image();
+    pacmanUpImage = "./pacmanUp.png";
+
+    pacmanDownImage = new Image();
+    pacmanDownImage = "./pacmanDownImage.png";
+
+    pacmanLeftImage = new Image();
+    pacmanLeftImage = "./pacmanLeftImage.png";
+
+    pacmanRightImage = new Image();
+    pacmanRightImage = "./pacmanRightImage.png"
+
+}
+
+function loadMap()
+{
+    walls.clear();
+    foods.clear();
+    ghosts.clear();
+
+    for (let r=0; r<rowCount; r++)
+    {
+        for (let c=0; c < columnCount; c++)
+        {
+            const row = tileMap[r];
+            const tileMapChar = row[c];
+
+            const x = c*tileSize;
+            const y = c*tileSize;
+
+            if (tileMapChar == 'X') 
+                { //block wall
+                const wall = new Block(wallImage, x, y, tileSize, tileSize);
+                walls.add(wall);  
+            }
+            else if (tileMapChar == 'b') 
+                { //blue ghost
+                const ghost = new Block(blueGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if (tileMapChar == 'o')
+                 { //orange ghost
+                const ghost = new Block(orangeGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if (tileMapChar == 'p') 
+                { //pink ghost
+                const ghost = new Block(pinkGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if (tileMapChar == 'r') 
+                { //red ghost
+                const ghost = new Block(redGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if (tileMapChar == 'P') 
+                { //pacman
+                pacman = new Block(pacmanRightImage, x, y, tileSize, tileSize);
+            }
+            else if (tileMapChar == ' ') 
+                { //empty is food
+                const food = new Block(null, x + 14, y + 14, 4, 4);
+                foods.add(food);
+            }
+        }
+    }
+}
+
+function update() {
+    draw();
+    setTimeout(update, 50);
+
+}
+
+function draw() {
+    context.drawImage(pacman.image, pacman.x, pacman.y, pacman.width, pacman.height);
+    for (let ghost of ghosts.values()) {
+        context.drawImage(ghost.image, ghost.y, ghost.width, ghost.height);
+    }
+}   
+
+class Block 
+{
+    constructor(image, x, y, width, height)
+    {
+        this.image = image ;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.startX = x;
+        this.startY = y;
+
+    }
 }
