c912fcef96ab640b1dc1653fe747a8c1220d3abe
samiyaazad <samiya16azad@gmail.com>
2026-02-05 19:33:43 +0600

    PacMan_Maze
    A web-based gameplay. The reason why I picked Pacman theme is Pacman is cute.

--- pacman.js
+++ pacman.js
@@ -18,21 +18,6 @@ let pacmanLeftImage;
 let pacmanRightImage;
 let wallImage;
 
-window.onload = function()
-{
-    board = document.getElementById("board");
-    board.height = boardHeight;
-    board.width = boardWidth;
-    context = board.getContext("2d");
-
-    loadImages();
-    loadMap();
-    //console.log(walls.size);
-    //console.log(foods.size);
-    //console.log(ghosts.size);
-    update();
-}
-
 //X = wall, O = skip, P = pac man, ' ' = food
 //Ghosts: b = blue, o = orange, p = pink, r = red
 const tileMap = [
@@ -64,9 +49,33 @@ const foods = new Set();
 const ghosts = new Set();
 let pacman;
 
+const directions = ['U', 'D', 'L', 'R'];
+let score = 0;
+let lives = 3;
+let gameOver = false;
 
-function loadImages()
-{
+
+window.onload = function(){
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); //used for drwaing on board 
+
+    loadImages();
+    loadMap();
+    //console.log(walls.size);
+    //console.log(foods.size);
+    //console.log(ghosts.size);
+    for (let ghost of ghosts.values()) {
+        const newDirection = directions[Math.floor(Math.random()*4)];
+        ghost.updateDirection(newDirection);
+    }
+    update();
+    document.addEventListener("keyup", movePacman);
+}
+
+
+function loadImages(){
     wallImage = new Image ();
     wallImage.src = "./wall.png";
 
@@ -83,16 +92,16 @@ function loadImages()
     orangeGhostImage.src = "./orangeGhost.png"
 
     pacmanUpImage = new Image();
-    pacmanUpImage = "./pacmanUp.png";
+    pacmanUpImage.src = "./pacmanUp.png";
 
     pacmanDownImage = new Image();
-    pacmanDownImage = "./pacmanDownImage.png";
+    pacmanDownImage.src = "./pacmanDown.png";
 
     pacmanLeftImage = new Image();
-    pacmanLeftImage = "./pacmanLeftImage.png";
+    pacmanLeftImage.src = "./pacmanLeft.png";
 
     pacmanRightImage = new Image();
-    pacmanRightImage = "./pacmanRightImage.png"
+    pacmanRightImage.src = "./pacmanRight.png"
 
 }
 
@@ -110,7 +119,7 @@ function loadMap()
             const tileMapChar = row[c];
 
             const x = c*tileSize;
-            const y = c*tileSize;
+            const y = r*tileSize;
 
             if (tileMapChar == 'X') 
                 { //block wall
@@ -151,17 +160,157 @@ function loadMap()
 }
 
 function update() {
+    if (gameOver) {
+        return;
+    }
+    move();
     draw();
-    setTimeout(update, 50);
+    setTimeout(update, 50);//1000/50 = 20 FPS
 
 }
 
 function draw() {
+    context.clearRect(0, 0, board.width, board.height); 
     context.drawImage(pacman.image, pacman.x, pacman.y, pacman.width, pacman.height);
     for (let ghost of ghosts.values()) {
-        context.drawImage(ghost.image, ghost.y, ghost.width, ghost.height);
+        context.drawImage(ghost.image, ghost.x, ghost.y, ghost.width, ghost.height);
     }
-}   
+    
+    for (let wall of walls.values()) {
+        context.drawImage(wall.image, wall.x, wall.y, wall.width, wall.height);
+    }
+
+    context.fillStyle = "white";
+    for (let food of foods.values()) {
+        context.fillRect(food.x, food.y, food.width, food.height);
+    }
+
+    context.fillStyle = "white";
+    context.font = "14px sans-serif";
+    if (gameOver) {
+        context.fillText("Game Over : " + String(score), tileSize/2, tileSize/2);
+    }
+    else {
+        context.fillText("x" + String(lives) + " " + String(score), tileSize/2, tileSize/2);
+    }
+
+}
+
+function move() {
+        pacman.x += pacman.velocityX;
+        pacman.y += pacman.velocityY;
+
+        for (let wall of  walls.values()) {
+            if (collision(pacman, wall)) {
+                pacman.x -= pacman.velocityX;
+                pacman.y -= pacman.velocityY;
+                break ;
+            }
+        }
+
+        for (let ghost of ghosts.values()) {
+
+            if (collision(ghost,pacman)) {
+                lives -= 1;
+                if(lives == 0) {
+                    gameOver = true;
+                    return;
+                }
+                resetPositions();
+            }
+
+            if (ghost.y == tileSize*9 && ghost.direction != 'U' && ghost.direction != 'D') {
+                ghost.updateDirection('U');
+            }
+            ghost.x += ghost.velocityX;
+            ghost.y += ghost.velocityY;
+            for(let wall of walls.values()) {
+                if (collision(ghost,wall) || ghost.x <= 0 || ghost.x + ghost.width >= boardWidth) {
+                    ghost.x -= ghost.velocityX;
+                    ghost.y -= ghost.velocityY;
+                    const newDirection = directions[Math.floor(Math.random()*4)];
+                    ghost.updateDirection(newDirection);
+                }
+            }
+        }
+
+        let foodEaten = null;
+        for (let food of foods.values()) {
+            if (collision(pacman, food)) {
+                foodEaten = food;
+                score += 10;
+                break;
+            }
+        }
+        foods.delete(foodEaten);
+
+        //
+
+        if (foods.size == 0) {
+            loadMap();
+            resetPositions();
+        }
+
+    }   
+    
+
+function movePacman(e) {
+    if(gameOver) {
+        loadMap();
+        resetPositions();
+        lives = 3;
+        score = 0;
+        gameOver = false;
+        update();
+        return;
+    }
+
+    if (e.code == "ArrowUp" || e.code == "KeyW") {
+        pacman.updateDirection("U");
+    }
+
+    else if (e.code == "ArrowDown" || e.code == "KeyS") {
+        pacman.updateDirection("D");
+    }
+    else if (e.code == "ArrowLeft" || e.code == "KeyA") {
+        pacman.updateDirection("L");
+    }
+    else if (e.code == "ArrowRight" || e.code == "KeyD") {
+        pacman.updateDirection("R");
+    }
+
+    if (pacman.direction == 'U') {
+        pacman.image = pacmanUpImage ;
+    }
+    else if (pacman.direction == 'D') {
+        pacman.image = pacmanDownImage ;
+    }
+    else if (pacman.direction == 'L') {
+        pacman.image = pacmanLeftImage ;
+    }
+    else if (pacman.direction == 'R') {
+        pacman.image = pacmanRightImage ;
+    }
+
+}
+
+function collision(a,b) {
+    return a.x < b.x + b.width && 
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
+}
+ 
+function resetPositions() {
+    pacman.reset();
+    pacman.velocityX = 0;
+    pacman.velocityY = 0;
+    for (let ghost of ghosts.values()) {
+        ghost.reset();
+        const newDirection = directions[Math.floor(Math.random()*4)];
+        ghost.updateDirection(newDirection);
+    }
+}
 
 class Block 
 {
@@ -176,5 +325,56 @@ class Block
         this.startX = x;
         this.startY = y;
 
+        this.direction = 'R';
+        this.velocityX = 0;
+        this.velocityY = 0;
+
     }
+
+    updateDirection(direction) {
+        const prevDirection = this.direction;
+        this.direction = direction;
+        this.updateVelocity();
+        this.x += this.velocityX;
+        this.y += this.velocityY;
+
+        for (let wall of walls.values()) {
+            if (collision(this,wall)) {
+                this.x -= this.velocityX;
+                this.y -= this.velocityY;
+                this.direction = prevDirection;
+                this.updateVelocity ();
+                return;
+            }
+        }
+
+    }
+
+    updateVelocity() {
+        if (this.direction == 'U') {
+            this.velocityX = 0;
+            this.velocityY = -tileSize/4;
+        }
+
+        else if (this.direction == 'D') {
+            this.velocityX = 0;
+            this.velocityY = tileSize/4;
+        }
+
+        else if (this.direction == 'L') {
+            this.velocityX = -tileSize/4;
+            this.velocityY = 0;
+        }
+
+        else if (this.direction == 'R') {
+            this.velocityX = tileSize/4;
+            this.velocityY = 0;
+        }
+
+    }
+    reset() {
+        this.x = this.startX;
+        this.y = this.startY;
+    }
+
 }
